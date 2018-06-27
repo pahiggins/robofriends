d81fb2dcb3f57c2d5cb348be886fded75d671639
Peter A. Higgins <pah@cenabo.com>
2018-06-27 14:26:40 +0100

    Add Card component

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 const Card = () => {
     return (
-        <div>Hello</div>
+        <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img src="https://robohash.org/test?200x200" alt="robot" />
+            <div>
+                <h2>Peter Higgins</h2>
+                <p>pah@cenabo.com</p>
+            </div>
+        </div>
     );
-}
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(<Card />, document.getElementById('root'));
 registerServiceWorker();
