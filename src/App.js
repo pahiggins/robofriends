ff2eec007bb5a51df537b99eb2c4233034e5827f
Peter A. Higgins <pah@cenabo.com>
2018-06-28 11:34:33 +0100

    Refactor SearchBox component and add some styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { robots } from './robots';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
+import './App.css';
 
 class App extends Component {
     constructor() {
@@ -24,7 +25,7 @@ class App extends Component {
 
         return (
             <div className="tc">
-                <h1>Robofriends</h1>
+                <h1 className="f1">Robofriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <CardList robots={filteredRobots} />
             </div>
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchBox extends Component {
-    render() {
-        return (
-            <div className="pa2">
-                <input
-                    className="pa3 ba b--green bg-lightest-blue"
-                    type="search"
-                    placeholder="Search Robots"
-                    onChange={this.props.searchChange}
-                />
-            </div>
-        );
-    }
+const SearchBox = (props) => {
+    return (
+        <div className="pa2">
+            <input
+                className="pa3 ba b--green bg-lightest-blue"
+                type="search"
+                placeholder="Search Robots"
+                onChange={props.searchChange}
+            />
+        </div>
+    );
 }
 
 export default SearchBox;
