7afc1660531671ee5baf4aeb10be54e8741187e9
Peter A. Higgins <pah@cenabo.com>
2018-06-28 12:00:48 +0100

    Add remote data source

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
 import './App.css';
@@ -8,10 +7,15 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots,
+            robots: [],
             searchText: ''
         };
         this.onSearchChange = this.onSearchChange.bind(this);
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => this.setState({ robots: users }));
     }
     onSearchChange(e) {
         this.setState({
@@ -23,13 +27,21 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchText);
         });
 
-        return (
-            <div className="tc">
-                <h1 className="f1">Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return (
+                <div className="tc">
+                    <h2>Loading...</h2>
+                </div>
+            );
+        } else {
+            return (
+                <div className="tc">
+                    <h2 className="f1">Robofriends</h2>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
