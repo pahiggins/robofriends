21264756f7ed746f95a09b0f605660a9adf54d4e
Peter A. Higgins <pah@cenabo.com>
2018-07-20 11:28:38 +0100

    Change Card component to use id for robot image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({ id, name, email }) => {
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-            <img src="https://robohash.org/test?200x200" alt="robot" />
+            <img src={`https://robohash.org/${id}?200x200`} alt="robot" />
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = (props) => {
-    return props.robots.map(robot => <Card key={robot.id} name={robot.name} email={robot.email} />);
+    return props.robots.map(robot => <Card key={robot.id} id={robot.id}  name={robot.name} email={robot.email} />);
 }
 
 export default CardList;
